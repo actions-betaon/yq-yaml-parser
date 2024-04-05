10cb3bb0650e0806f2835cbbbe3852d5ddf7454f
Jairon Alves Lima <jairon.alves.lima@gmail.com>
2024-04-04 23:53:49 -0300

    feat: v1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,23 @@
 #!/bin/sh -l
 
+_yaml_to_properties() {
+  local yaml_file="$1"
+  yq -o p --properties-separator ':' '... comments = ""' "$yaml_file"
+}
+
+_replace_dots() {
+  local string="$1"
+  local replacement="$2"
+  echo "$string" | awk -v rep="$replacement" 'BEGIN{FS=OFS=":"} {gsub(/\./,rep,$1); print}'
+}
+
 set -e
 
 # Read the file using yq
-yq e "$1"
+yq -o p --properties-separator ':' '... comments = ""' "$1"
+
+_properties=$(_yaml_to_properties "$YAMLPARSER_FILE_PATH")
+_parsed_properties=$(_replace_dots "$properties" "_")
 
 # Use INPUT_<INPUT_NAME> to get the value of an input
 GREETING="Hello, $INPUT_WHO_TO_GREET!"
