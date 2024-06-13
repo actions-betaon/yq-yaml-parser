0632d9dd493501e8b780daf26d100f38b5da5d2b
Jairon Alves Lima <jairon.alves.lima@gmail.com>
2024-06-12 22:39:46 -0300

    feat: v1

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,16 +13,16 @@ _replace_dots() {
 
 set -e
 
-#_properties=$(_yaml_to_properties "$INPUT_YAML_FILE_PATH")
-#_parsed_properties=$(_replace_dots "$properties" "_")
+_properties=$(_yaml_to_properties "$INPUT_YAML_FILE_PATH")
+_parsed_properties=$(_replace_dots "$properties" "_")
 
 # Use INPUT_<INPUT_NAME> to get the value of an input
 GREETING="Hello, $INPUT_WHO_TO_GREET!"
 
 # Use workflow commands to do things like set debug messages
-#echo "::notice file=entrypoint.sh,line=23::$GREETING"
+echo "::notice file=entrypoint.sh,line=23::$GREETING"
 
 # Write outputs to the $GITHUB_OUTPUT file
-#echo "time=$(date)" >>"$GITHUB_OUTPUT"
+echo "time=$(date)" >>"$GITHUB_OUTPUT"
 
 exit 0
