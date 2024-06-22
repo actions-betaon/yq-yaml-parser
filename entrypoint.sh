fc3859aebe1cc295277eaa9edaf6135373d167e5
Jairon Alves Lima <jairon.alves.lima@gmail.com>
2024-06-22 16:47:31 -0300

    feat: v1

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,7 @@ _set_github_output() {
   local propAndValue="$1"
   prop="${propAndValue%%=*}"
   value="${propAndValue#*=}"
-  #echo "${propAndValue}"
+  echo "${propAndValue}"
   #echo "$(printf '%b\n' "$value")"
   if echo $value | grep -iq "\\n"; then
     value_multiline=$(echo "${value//#EOL#/$'\n'}")
@@ -35,7 +35,7 @@ _properties=$(_yaml_to_properties "$INPUT_YAML_FILE_PATH")
 _parsed_properties=$(_replace_dots "$_properties" "_")
 #_escaped_multiline_properties=$(echo "${_parsed_properties//\\n/#EOL#}")
 
-echo "$(printf '%b\n' "$_parsed_properties")"
+#echo "$(printf '%b\n' "$_parsed_properties")"
 
 echo "$_parsed_properties" | while read -r propAndValue;
 do
