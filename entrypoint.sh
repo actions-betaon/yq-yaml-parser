cb472ef44ca0c9f3ff7e91e020da9de069fd64bc
Jairon Alves Lima <jairon.alves.lima@gmail.com>
2024-06-22 18:15:20 -0300

    feat: v1

--- entrypoint.sh
+++ entrypoint.sh
@@ -40,12 +40,13 @@ _set_github_output() {
 set -e
 
 _properties=$(_yaml_to_properties "$INPUT_YAML_FILE_PATH")
+_escaped_multiline_properties=$(echo "${_properties//\\n/#EOL#}")
 #_properties_print="$(printf '%b\n' "$_properties")"
 #_parsed_properties=$(_replace_dots "$_properties" "_")
 #_escaped_multiline_properties=$(echo "${_parsed_properties//\\n/#EOL#}")
 
 #properties_ec=$(echo "$(printf '%b\n' "$_properties")")
-echo "$_properties"
+echo "$_escaped_multiline_properties"
 #echo "$(printf '%b\n' "$_properties")"
 #echo -e "$_properties_print"
 
