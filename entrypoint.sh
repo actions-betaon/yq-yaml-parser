af39341533dd71ed698dce4b6173177ad5baff68
Jairon Alves Lima <jairon.alves.lima@gmail.com>
2024-07-09 23:16:27 -0300

    feat: Linter

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,52 +2,52 @@
 # shellcheck shell=dash
 
 _yaml_to_properties() {
-  local yaml_file="$1"
-  yq -o p --properties-separator '=' '... comments = ""' "$yaml_file"
+	local yaml_file="$1"
+	yq -o p --properties-separator '=' '... comments = ""' "$yaml_file"
 }
 
 _replace_dots() {
-  local string="$1"
-  local replacement="$2"
-  echo "${string}" | sed "s/\./${replacement}/g"
+	local string="$1"
+	local replacement="$2"
+	echo "${string}" | sed "s/\./${replacement}/g"
 }
 
 _property_value_to_multiline() {
-  local propertyValue="$1"
-  local lineMark="#LN#"
+	local propertyValue="$1"
+	local lineMark="#LN#"
 
-  propertyValueMultiLine=$(echo "$propertyValue" | sed "s/\\\\n/$lineMark/g")
-  propertyValueMultiLine=$(echo "$propertyValueMultiLine" | sed 's/\\/\\\\/g')
-  propertyValueMultiLine=$(echo "$propertyValueMultiLine" | sed "s/$lineMark/\n/g")
-  propertyValueMultiLine=$(echo "$propertyValueMultiLine" | sed '${/^$/d;}')
-  echo "$propertyValueMultiLine"
+	propertyValueMultiLine=$(echo "$propertyValue" | sed "s/\\\\n/$lineMark/g")
+	propertyValueMultiLine=$(echo "$propertyValueMultiLine" | sed 's/\\/\\\\/g')
+	propertyValueMultiLine=$(echo "$propertyValueMultiLine" | sed "s/$lineMark/\n/g")
+	propertyValueMultiLine=$(echo "$propertyValueMultiLine" | sed '${/^$/d;}')
+	echo "$propertyValueMultiLine"
 }
 
 _set_github_output() {
-  local propertyName="$1"
-  local propertyValue="$2"
+	local propertyName="$1"
+	local propertyValue="$2"
 
-  if echo "$propertyValue" | grep -q '\\n'; then
-    propertyValueMultiLine=$(_property_value_to_multiline "$propertyValue")
-    {
-      echo "$propertyName<<EOF"
-      printf "%b\n" "$propertyValueMultiLine"
-      echo "EOF"
-    } >>"$GITHUB_OUTPUT"
-  else
-    echo "$propertyName=$propertyValue" >>"$GITHUB_OUTPUT"
-  fi
+	if echo "$propertyValue" | grep -q '\\n'; then
+		propertyValueMultiLine=$(_property_value_to_multiline "$propertyValue")
+		{
+			echo "$propertyName<<EOF"
+			printf "%b\n" "$propertyValueMultiLine"
+			echo "EOF"
+		} >>"$GITHUB_OUTPUT"
+	else
+		echo "$propertyName=$propertyValue" >>"$GITHUB_OUTPUT"
+	fi
 }
 
 _set_github_outputs() {
-  local properties="$1"
-  local propertyNameDotReplace="$2"
+	local properties="$1"
+	local propertyNameDotReplace="$2"
 
-  echo "$properties" | while read -r propertyLine; do
-    propertyName=$(_replace_dots "${propertyLine%%=*}" "$propertyNameDotReplace")
-    propertyValue="${propertyLine#*=}"
-    _set_github_output "$propertyName" "$propertyValue"
-  done
+	echo "$properties" | while read -r propertyLine; do
+		propertyName=$(_replace_dots "${propertyLine%%=*}" "$propertyNameDotReplace")
+		propertyValue="${propertyLine#*=}"
+		_set_github_output "$propertyName" "$propertyValue"
+	done
 }
 
 set -e
